1c608a746fb5557788e785168e57fc45b5d4aa86
Crescendi <martahonolulu@hotmail.com>
2016-09-12 17:08:18 -1000

    wip 15 test passed

--- linkedList.js
+++ linkedList.js
@@ -4,5 +4,53 @@
  * @return {Object} an object exposing methods to be used to manipulate a linked list
  */
 function linkedListGenerator(){
+  let _linkedList = {};
+  let _head = null;
+  let _tail = null;
 
+  //define the head like empty node
+  //define the tail like empty node
+
+
+
+return {
+  getHead: function () {
+    return _head;
+  },
+  getTail: function () {
+    return _tail;
+  },
+  add: function (value) {
+    let _newNode = {};
+    _newNode.value = value;
+    _newNode.next = null;
+    if (_head === null) {
+      _head = _newNode; //{ value next: null}
+      _tail = _newNode;
+    } else {
+        _tail.next = _newNode; //
+        _tail = _newNode; // 3. step
+      }
+    return _newNode;
+  },
+  remove: function (number) {
+    // body...
+  },
+  get: function (number) {
+    // body...
+  },
+  insert: function (value, number) {
+    // body...
+  }
+};
 }
+
+//let listGen = linkedListGenerator();
+
+//let myHead = listGen.getHead();
+
+
+
+  // add
+  // add.()
+
