52913c90f98e7d70322d93fa414b769a7afc4bc7
Crescendi <martahonolulu@hotmail.com>
2016-09-13 14:20:08 -1000

    done

--- linkedList.js
+++ linkedList.js
@@ -4,8 +4,8 @@
  * @return {Object} an object exposing methods to be used to manipulate a linked list
  */
 function linkedListGenerator(){
-  let _linkedList = {};
-  let _head = null;
+  //let _linkedList = {};
+  let _head = null; // _ mans private
   let _tail = null;
 
   //define the head like empty node
@@ -21,17 +21,17 @@ return {
     return _tail;
   },
   add: function (value) {
-    let _newNode = {};
-    _newNode.value = value;
-    _newNode.next = null;
+    let newNode = {};
+    newNode.value = value;
+    newNode.next = null;
     if (_head === null) {
-      _head = _newNode; //{ value next: null}
-      _tail = _newNode;
+      _head = newNode; //{ value next: null}
+      _tail = newNode;
     } else {
-        _tail.next = _newNode; //
-        _tail = _newNode; // 3. step
+        _tail.next = newNode; //
+        _tail = newNode; // 3. step
       }
-    return _newNode;
+    return newNode;
   },
   remove: function (index) {
     const indexNode = this.get(index);
@@ -55,45 +55,79 @@ return {
     }
   },
   get: function (index) {
-    let counter = 0;
-    let foundNode = {};
-    foundNode.value = _head.value;
-    foundNode.next = _head.next;
+    let currPointer = 0;
+    let currNode = {};
+    currNode.value = _head.value;
+    currNode.next = _head.next;
 
-    while ( counter < index && foundNode.next !== null) {
-        foundNode = foundNode.next;
-        counter ++;
+    while ( currPointer < index && currNode.next !== null) {
+      currNode = currNode.next;
+      currPointer ++;
     }
 
-    if (counter === index) {
-      return foundNode;
+    if (currPointer === index) {
+      return currNode;
     } else {
+        return false;
+    }
+
+    if (index < 0){
       return false;
     }
   },
   insert: function (value, index) {
-    const insertNode = {};
-    const beforeNode = this.get(index-1);
-    console.log('value and index: ', value, index);
-    console.log('before node: ', beforeNode);
-
-    if ((beforeNode.next) !== null && (index > 0)) {
-      insertNode.value = value;
-      insertNode.next = beforeNode.next;
-      console.log('insertNode after put next:::', insertNode);
-      beforeNode.next = insertNode;
-      console.log('inside first if after insert before Node:', beforeNode);
-      console.log('inside first if after insert Insert Node:', insertNode.next);
-    } else {
-      if (index === 0) {
-        insertNode.next = null;
-        head.next = insertNode;
-        _head = insertNode;
-      } else {
-          return false;
-        }
+    let currNode = this.get(index);
+    let prevNode = this.get(index-1);
+    let newNode = {
+      value: value,
+      next: null
+    };
+    if (currNode === false) {
       return false;
     }
+    if (index === 0) {
+      newNode.next = _head;
+      _head = newNode;
+    } else {
+        newNode.next = currNode;
+        prevNode.next = newNode;
+        _head = prevNode;
+
+    }
+    if (currNode.next === null) {
+        _tail = newNode;
+        currNode.next = newNode;
+    } else {
+        prevNode.next = newNode;
+        newNode.next = currNode;
+    }
+
+
+
+// const insertNode = {};
+    // const beforeNode = this.get(index-1);
+    // console.log('value and index: ', value, index);
+    // console.log('before node before manipulation : ', beforeNode);
+
+    // if ((beforeNode.next) !== null ) { //&& (index > 0)
+    //   insertNode.value = value;
+    //   insertNode.next = beforeNode.next;
+    //   console.log('insertNode after put next:::', insertNode);
+    //   beforeNode.next = insertNode;
+    //   console.log('inside first if after insert before Node:', beforeNode);
+    //   console.log('inside first if after insert Insert Node:', insertNode);
+    //   if ((index-1) === 0 ) {
+    //      _head = beforeNode;
+    //   }
+    //   if (index === 0) {
+    //      insertNode.next = null;
+    //      head.next = insertNode;
+    //      _head = insertNode;
+    //   }
+    // } else {
+    //     return false;
+    //  }
+
   } // eof insert
 };
 }
